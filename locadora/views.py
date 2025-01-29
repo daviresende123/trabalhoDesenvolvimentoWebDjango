e2e3e77ae6db61d3809d477d6c6e1c29a1cd31c1
Davi Resende <davi.resende@estudante.ufla.br>
2025-01-29 16:17:02 -0300

    login de usuario criado

--- locadora/views.py
+++ locadora/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, 'locadora/home.html')
+
+@login_required
+def perfil(request):
+    return render(request, 'locadora/perfil.html', {'user': request.user})
+
+def registrar(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registrar.html', {'form': form})
